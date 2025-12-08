80680e1dcda4ecd731678f83aa77e8f61bb04cae
Rubén Noriega Mier <rubennoriegamier@gmail.com>
2025-12-08 09:44:27 +0100

    Refactor with deque

--- aoc/day_08.py
+++ aoc/day_08.py
@@ -1,19 +1,16 @@
 import fileinput
+from collections import deque
 from dataclasses import dataclass
 from functools import reduce
 from itertools import combinations
 from operator import mul
-from time import perf_counter
 from typing import Self
 
 
 def main():
     junk_boxes: list[JunkBox] = list(map(JunkBox.parse, fileinput.input()))
 
-    t_1 = perf_counter()
     print(*part_1_and_2(junk_boxes, 1_000), sep='\n')
-    t_2 = perf_counter()
-    print(round((t_2 - t_1) * 1_000, 3))
 
 
 @dataclass(frozen=True, slots=True)
@@ -32,22 +29,20 @@ class JunkBox:
 
 def part_1_and_2(junk_boxes: list[JunkBox], pairs_count: int) -> tuple[int, int]:
     pairs = sorted(combinations(junk_boxes, 2), key=lambda pair: pair[0].distance_to(pair[1]))
-    networks = []
+    networks = deque()
     part_1 = 0
     part_2 = 0
 
     for i, (jb_a, jb_b) in enumerate(pairs, 1):
-        networks_ = []
-        network_a = {jb_a, jb_b}
+        network = {jb_a, jb_b}
 
-        for network_b in networks:
-            if network_a.isdisjoint(network_b):
-                networks_.append(network_b)
+        for _ in range(len(networks)):
+            if network.isdisjoint(networks[0]):
+                networks.rotate(-1)
             else:
-                network_a.update(network_b)
-        networks_.append(network_a)
-
-        networks = networks_
+                network.update(networks[0])
+                del networks[0]
+        networks.append(network)
 
         if i == pairs_count:
             part_1 = reduce(mul, sorted(map(len, networks))[-3:])
