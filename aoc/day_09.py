f71eeee6e7b786b1c3eca7fee2c850594644587a
Rubén Noriega Mier <rubennoriegamier@gmail.com>
2025-12-10 02:52:39 +0100

    Optimization

--- aoc/day_09.py
+++ aoc/day_09.py
@@ -29,14 +29,16 @@ def part_2(tiles: list[Tile]) -> int | None:
     sorted_segments = sorted(((min(x_3, x_4), min(y_3, y_4)),
                               (max(x_3, x_4), max(y_3, y_4)))
                              for (x_3, y_3), (x_4, y_4) in pairwise(tiles + [tiles[0]]))
+    next_segments = {x: [sg for sg in sorted_segments if sg[1][0] > x]
+                     for (_, _), (x, _) in sorted_segments}
 
     # noinspection PyInconsistentReturns
     for (x_1, y_1), (x_2, y_2) in rects:
         x_1, x_2 = sorted([x_1, x_2])
         y_1, y_2 = sorted([y_1, y_2])
 
-        for (x_3, y_3), (x_4, y_4) in sorted_segments:
-            if x_4 > x_1 and x_3 < x_2 and y_4 > y_1 and y_3 < y_2:
+        for (x_3, y_3), (_, y_4) in next_segments[x_1]:
+            if x_3 < x_2 and y_4 > y_1 and y_3 < y_2:
                 break
         else:
             return (x_2 - x_1 + 1) * (y_2 - y_1 + 1)
